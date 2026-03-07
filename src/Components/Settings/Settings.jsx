9a8090b938edec7f5e8c9e2e028f2ad5f3af5916
Tinsae Temesgen <tinsutemesgen2@gmail.com>
2026-03-08 01:17:26 +0300

    Setting page adjestments

--- src/Components/Settings/Settings.jsx
+++ src/Components/Settings/Settings.jsx
@@ -1,4 +1,3 @@
-import { Routes, Route } from "react-router-dom";
 import { FiMoon, FiSun, FiArrowLeft } from "react-icons/fi";
 import { Link } from 'react-router-dom';
 import './Settings.css';
@@ -9,25 +8,28 @@ function Settings() {
 
     <div className="setting-container">
       <div className="settings-page">
-              <div className="link">
+          <div className="link">
             <Link to="/" className="back-link">
             <FiArrowLeft />Back to Dashboard</Link>
         </div>
 
         <h1>Settings</h1>
-        <p>Manage your profile, account settings, and application preferences here.</p>
+        <p>Manage your profile settings and application preferences here.</p>
         
-        <div className="theme-option">
-          <input type="radio" id="light" name="theme" />
-            <label htmlFor="light">
-            <FiSun /> Light
-          </label>
+          <div className="theme-option">
+            <h2>Theme Preference</h2>
 
-          <input type="radio" id="dark" name="theme" />
-          <label htmlFor="dark">
-            <FiMoon /> Dark
-          </label>
-        </div>
+            <label className="theme-toggle">
+              <FiSun className="sun" />
+
+              <input type="checkbox" id="theme" />
+
+              <span className="slider"></span>
+
+              <FiMoon className="moon" />
+            </label>
+
+          </div>
 
         <div className="language">
           <h2>Language</h2>
@@ -38,6 +40,21 @@ function Settings() {
             <option value="amharic">Amharic</option>
           </select>
         </div>
+
+
+
+        <div className="font">
+          <h2>Font Size</h2>
+
+          <input type="radio" />
+          <label htmlFor="small">Small</label>
+          <input type="radio" />
+          <label htmlFor="medium">Medium</label>
+          <input type="radio" />
+          <label htmlFor="large">Large</label>
+
+        </div>
+
     </div>
     </div>
     
