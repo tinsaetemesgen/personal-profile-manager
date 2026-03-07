523c60a990d838ef9342920ad316997fbbb8bc9a
Tinsae Temesgen <tinsutemesgen2@gmail.com>
2026-03-07 11:24:20 +0300

    Setting page layout

--- src/Components/Settings/Settings.jsx
+++ src/Components/Settings/Settings.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
-import { FiHome, FiUser, FiTool, FiFolder, FiSettings, FiLogOut, FiArrowLeftCircle, FiArrowDownLeft, FiArrowLeft } from "react-icons/fi";
+import { FiMoon, FiSun, FiArrowLeft } from "react-icons/fi";
 import { Link } from 'react-router-dom';
 import './Settings.css';
 
 
 function Settings() {
   return (
-    <div className="settings-page">
+
+    <div className="setting-container">
+      <div className="settings-page">
               <div className="link">
             <Link to="/" className="back-link">
             <FiArrowLeft />Back to Dashboard</Link>
@@ -15,7 +17,30 @@ function Settings() {
         <h1>Settings</h1>
         <p>Manage your profile, account settings, and application preferences here.</p>
         
+        <div className="theme-option">
+          <input type="radio" id="light" name="theme" />
+            <label htmlFor="light">
+            <FiSun /> Light
+          </label>
+
+          <input type="radio" id="dark" name="theme" />
+          <label htmlFor="dark">
+            <FiMoon /> Dark
+          </label>
+        </div>
+
+        <div className="language">
+          <h2>Language</h2>
+          <select name="lang" id="">
+            <option value="englishus">English (us)</option>
+            <option value="englishuk">English (uk)</option>
+            <option value="french">French</option>
+            <option value="amharic">Amharic</option>
+          </select>
+        </div>
     </div>
+    </div>
+    
   )
 }
 
